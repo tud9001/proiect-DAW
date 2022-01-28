652583ac8f3e8b292f324c73c7540dd809e693ba
unknown <rodut13@gmail.com>
2022-01-28 05:23:43 +0200

    adaugat sample de database

--- proiect DAW/Persistance/DataContext.cs
+++ proiect DAW/Persistance/DataContext.cs
@@ -9,5 +9,17 @@ namespace Persistance
         public DataContext(DbContextOptions options): base(options)
         {}
         public DbSet<value> Values {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder builder){
+            builder.Entity<value>()
+            .HasData(
+                new value {Id = 1, Name = "mare"},
+                
+                new value {Id = 1, Name = "schema"},
+                
+                new value {Id = 1, Name = "aicea"}
+            );
+        }
+        
     }
 }
