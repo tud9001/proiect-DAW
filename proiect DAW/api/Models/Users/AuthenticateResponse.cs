a0ebf98361e20cfde21c2ff7df7f708265b5bbc3
unknown <rodut13@gmail.com>
2022-02-01 22:46:11 +0200

    autentificare 2

--- proiect DAW/api/Models/Users/AuthenticateResponse.cs
+++ proiect DAW/api/Models/Users/AuthenticateResponse.cs
@@ -1,7 +1,26 @@
-namespace api.Models.Users
+using System;
+using System.Collections.Generic;
+using Domain;
+
+namespace WebApi.Models.Users
 {
     public class AuthenticateResponse
     {
-        
+        public Guid Id{get; set;}
+        public string Name{get;set;}
+        public float Balance{get;set;}
+        public int Idlogin{get; set;}
+        public role role{get; set;}
+        public virtual ICollection<achizitii> achizitie { get; set; }
+        public string Token { get; set; }
+
+        public AuthenticateResponse(user user, string token)
+        {
+            Id = user.Id;
+            Name=user.Name;
+            Balance = user.Balance;
+            role = user.role;
+            Token = token;
+        }
     }
 }
