956edacabb8e9bb07a3171c56531cbd7241db8ab
unknown <rodut13@gmail.com>
2022-02-01 02:10:47 +0200

    am adaugat tabele

---
+++ proiect DAW/Domain/login.cs
@@ -0,0 +1,8 @@
+namespace Domain
+{
+    public class login
+    {
+        public int Idlogin{get; set;}
+        public int parola{get; set;}
+    }
+}
---
+++ proiect DAW/Domain/producator.cs
@@ -0,0 +1,8 @@
+namespace Domain
+{
+    public class producator
+    {
+        public int Id{get; set;}
+        public int nume{get; set;}
+    }
+}
